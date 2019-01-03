36a56148e55ea772814cca4cf323604fbf548a95
FeLO <felo.dawid@gmail.com>
2019-01-04 00:31:32 +0100

    Test fix

---
+++ Scripts/clearPolicja.sql
@@ -0,0 +1,14 @@
+use Policja
+go
+
+
+DELETE Dane_patrolu
+DELETE Funkcjonariusz
+DELETE Miejsce_zdarzenia
+DELETE Osoba_legitymowana
+DELETE Patrol
+DELETE Powod
+DELETE Wystawianie_mandatow
+
+DELETE Data
+DELETE Czas
