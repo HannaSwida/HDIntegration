2f14f4c41a0a2f31748155b65773a0506edafdf8
FeLO <felo.dawid@gmail.com>
2019-01-03 14:49:44 +0100

    Final, bez Id_patrolu

--- Scripts/swieta.sql
+++ Scripts/swieta.sql
@@ -1,5 +1,19 @@
 use tempHD
-go
+
+insert into dbo.swieta ("data") values ('2017-01-01');
+insert into dbo.swieta ("data") values ('2017-01-06');
+insert into dbo.swieta ("data") values ('2017-04-01');
+insert into dbo.swieta ("data") values ('2017-04-02');
+insert into dbo.swieta ("data") values ('2017-05-01');
+insert into dbo.swieta ("data") values ('2017-05-03');
+insert into dbo.swieta ("data") values ('2017-05-20');
+insert into dbo.swieta ("data") values ('2017-05-31');
+insert into dbo.swieta ("data") values ('2017-08-15');
+insert into dbo.swieta ("data") values ('2017-11-01');
+insert into dbo.swieta ("data") values ('2017-11-11');
+insert into dbo.swieta ("data") values ('2017-12-25');
+insert into dbo.swieta ("data") values ('2017-12-26');
+
 insert into dbo.swieta ("data") values ('2018-01-01');
 insert into dbo.swieta ("data") values ('2018-01-06');
 insert into dbo.swieta ("data") values ('2018-04-01');
@@ -28,3 +42,5 @@ insert into dbo.swieta ("data") values ('2019-11-11');
 insert into dbo.swieta ("data") values ('2019-12-25');
 insert into dbo.swieta ("data") values ('2019-12-26');
 
+go
+use master
