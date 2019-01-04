b04ca9b7a633a88c566d2a49d1919224d27bda9c
FeLO <felo.dawid@gmail.com>
2019-01-04 12:09:01 +0100

    Final

--- Scripts/clearPolicja.sql
+++ Scripts/clearPolicja.sql
@@ -1,14 +1,23 @@
 use Policja
 go
 
+DELETE FROM Wystawianie_mandatow
+DBCC CHECKIDENT ('Wystawianie_mandatow',RESEED, 0)
+DELETE FROM Miejsce_zdarzenia
+DBCC CHECKIDENT ('Miejsce_zdarzenia',RESEED, 0)
+DELETE FROM Funkcjonariusz
+DBCC CHECKIDENT ('Funkcjonariusz',RESEED, 0)
 
-DELETE Dane_patrolu
-DELETE Funkcjonariusz
-DELETE Miejsce_zdarzenia
-DELETE Osoba_legitymowana
-DELETE Patrol
-DELETE Powod
-DELETE Wystawianie_mandatow
+DELETE FROM Osoba_legitymowana
+DBCC CHECKIDENT ('Osoba_legitymowana',RESEED, 0)
+DELETE FROM Patrol
+DBCC CHECKIDENT ('Patrol',RESEED, 0)
+DELETE FROM Dane_patrolu
+DBCC CHECKIDENT ('Dane_patrolu',RESEED, 0)
+DELETE FROM Powod
+DBCC CHECKIDENT ('Powod',RESEED, 0)
 
-DELETE Data
-DELETE Czas
+
+
+TRUNCATE TABLE Data
+TRUNCATE TABLE Czas
